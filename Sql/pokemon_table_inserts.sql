e041504ec9049fce578ce52955340f0d6b49bb1c
Meri <andreeaanamariaioan@gmail.com>
2020-01-31 16:08:46 +0200

    inserts for type table done + inserts for pokemon prepared

---
+++ Sql/pokemon_table_inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO `pokemon` (`name`, `type_id`, `generation_id`, `max_cp`, `max_hp`, `max_attack`, `max_defense`, `nr_candy_evolve`, `shiny`, `buddy_km`, `egg_km`, `level_max_cp`, `second_charge_attack_id`, `extra_info`)
+VALUES
+();
---
+++ Sql/type_table_inserts.sql
@@ -0,0 +1,20 @@
+INSERT INTO `type` (`type`)
+VALUES
+(`normal`),
+(`fire`),
+(`bug`),
+(`poison`),
+(`flying`),
+(`dark`),
+(`psychic`),
+(`ghost`),
+(`dragon`),
+(`rock`),
+(`grass`),
+(`electric`),
+(`steel`),
+(`ground`),
+(`fairy`),
+(`water`),
+(`ice`),
+(`fighting`);
